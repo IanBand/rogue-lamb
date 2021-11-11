2adf63c58639eb572984220a45d31047f285be09
ianband <ian.r.band@gmail.com>
2021-11-11 11:30:24 -0800

    Init

---
+++ app/Main.hs
@@ -0,0 +1,140 @@
+module Main where
+    
+import System.IO
+import Data.Maybe
+import Control.Concurrent
+import System.Console.ANSI
+
+
+-- import System.TimeIt
+
+-- advanceFrame :: GlobalState -> Inputs -> GlobalState
+
+data TextBoxPosition = Top | Middle | Bottom deriving (Show)
+
+data TextBox = TextBox {
+    -- charPerFrame :: Natural, -- we aint usin this our first time round... must keep in mind frames will only be rendered upon user input (wasd + numbers)
+    contents :: String,
+    position :: TextBoxPosition
+} deriving (Show)
+
+type Coordinate = (Int, Int)
+data MapId = Home | RivalHome | StartingTown | StartLab deriving(Show, Eq)
+
+data ColissionType = Ground | Encounter | Wall | Warp MapId Coordinate | Water deriving(Show)
+
+data TileRender = TileRender {
+    -- global frame number mod list length... or they could be infinite lists??? nah
+    charColorList :: [System.Console.ANSI.Color],
+    backgroundColorList :: [System.Console.ANSI.Color],
+    characterList   :: [Char],
+    italicizedList  :: [Bool]
+} deriving (Show)
+data Tile = Tile {
+    render :: TileRender,
+    colission :: ColissionType
+} deriving (Show)
+
+data SceneType = InMenu | InTextBox | InOverworld | InBattle deriving(Show)
+
+data Cardinal = North | South | East | West deriving(Show)
+
+data OverworldState = OverworldState {
+    playerFacing :: Cardinal,
+    playerCoordinate :: Coordinate,
+    currentMap :: MapId
+    -- list of current actors + positions?
+} deriving(Show)
+data GameState = GameState {
+    currentSceneStack :: [SceneType], -- use a real stack template or whatever its called (ADT?) in haskell
+    overworldState :: OverworldState
+    -- team :: (Ally, Ally, Ally, Ally, Ally),
+    -- inventory :: (Item, Item, Item, Item ... ),
+    -- 
+    -- battleState :: BattleState,
+    -- menuState :: MenuState,
+    -- textBoxState :: TextBoxState,
+    -- gameFlags :: GameFlags
+
+} deriving (Show)
+
+data GameInput = Up | Down | Left | Right | Advance | Back | MenuInput deriving(Show)
+-- data VirtualInput ... do we need this layer?
+
+computeNextState :: GameState -> GameInput -> GameState
+computeNextState prevState inputs = prevState
+
+drawState :: GameState -> [IO ()]
+drawState state = [return ()]
+
+
+-- drawTextBox :: ScreenBuf -> TextBox -> ScreenBuf
+
+{-
+blankBuffer = blankBufferNxN 16 16
+blankBufferNxN :: Int -> Int -> ScreenBuf
+blankBufferNxN h w
+    | h <= 0 = blankBufferNxN 64 w
+    | w <= 0 = blankBufferNxN h 64
+    | otherwise = replicate w col where col = replicate h '.'
+
+
+screenBufToString :: ScreenBuf -> [Char]
+screenBufToString  buf = (foldl1 (\acc line -> acc ++ "\n" ++ line) buf) ++ "\n"
+--printScreenBuf buf = map (\l -> l ++ "\n") buf 
+
+-}
+
+
+-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= --
+
+
+ifReadyDo :: Handle -> IO a -> IO (Maybe a)
+ifReadyDo hnd x = hReady hnd >>= f
+   where f True = x >>= return . Just
+         f _    = return Nothing
+
+-- https://stackoverflow.com/questions/3894792/what-is-a-simple-way-to-wait-for-and-then-detect-keypresses-in-haskell
+-- https://stackoverflow.com/questions/7829574/simple-counter-in-io
+-- define an infinite list of frames to execute...?
+
+-- https://stackoverflow.com/questions/2472391/how-do-i-clear-the-terminal-screen-in-haskell
+-- https://hackage.haskell.org/package/ansi-terminal-0.5.0/docs/System-Console-ANSI.html
+
+{-
+clear = putStr "\ESC[2J"
+
+main = do
+    hSetEcho stdin False
+    hSetBuffering stdin NoBuffering
+    input <- stdin `ifReadyDo` getChar
+    if input == Nothing
+        then return ()
+        else do
+            putStr ( [fromJust input] ++ "+")
+    putStr $ screenBufToString blankBuffer
+    threadDelay 1000000 -- 16666 -- 16.6ms, 60fps
+    clear
+    
+    main
+    
+-}
+
+main :: IO ()
+main = do
+    setCursorPosition 5 0
+    setTitle "ANSI Terminal Short Example"
+
+    setSGR [ SetConsoleIntensity FaintIntensity
+           , SetColor Foreground Vivid Red
+           ]
+    putStr "Hello"
+
+    setSGR [ SetConsoleIntensity FaintIntensity
+           , SetColor Foreground Vivid White
+           , SetColor Background Dull Blue
+           ]
+    putStrLn "World!"
+    threadDelay 1000000 
+    hClearScreen stdout
+
